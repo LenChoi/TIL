1b9be8985f8cd84f911721ddb2037333573b4b41
LenChoi <clilc@naver.com>
2023-02-28 17:16:42 +0900

    불필요한 파일 제거

--- redis-performance/src/main/kotlin/com/example/redisperformance/service/ProductServiceV1.kt
+++ redis-performance/src/main/kotlin/com/example/redisperformance/service/ProductServiceV1.kt
@@ -18,3 +18,38 @@ class ProductServiceV1(
             .flatMap(productRepository::save)
     }
 }
+
+class Solution {
+    fun solution(cards1: Array<String>, cards2: Array<String>, goal: Array<String>): String {
+        var answer = ""
+        val cards1List = cards1.toMutableList()
+        val cards2List = cards2.toMutableList()
+
+        goal.forEach {
+            answer = "No"
+            if (cards1List.firstOrNull() == it) {
+                cards1List.removeAt(0)
+                answer = "Yes"
+            }
+
+            if (cards2List.firstOrNull() == it) {
+                cards2List.removeAt(0)
+                answer = "Yes"
+            }
+
+            if (answer == "No") return answer
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    val solution = Solution()
+
+    val result = solution.solution(
+        arrayOf("i", "water", "drink"), arrayOf("want", "to"), arrayOf("i", "want", "to", "drink", "water")
+    )
+
+    println(result)
+}
