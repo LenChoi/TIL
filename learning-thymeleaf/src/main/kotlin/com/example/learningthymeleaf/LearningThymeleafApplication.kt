87572b422a3e108a3237340ca44f1840b81c2c43
len.choi <len.choi@cyworldlabs.com>
2023-04-04 20:38:27 +0900

    learning-thymeleaf 프로젝트 생성

---
+++ learning-thymeleaf/src/main/kotlin/com/example/learningthymeleaf/LearningThymeleafApplication.kt
@@ -0,0 +1,11 @@
+package com.example.learningthymeleaf
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LearningThymeleafApplication
+
+fun main(args: Array<String>) {
+    runApplication<LearningThymeleafApplication>(*args)
+}
